dda8d882d8dc08eb5e4411cbee163d6b46713bd8
camilo-OG <camilo.osorio.gsach.cl>
2023-10-31 22:20:26 -0300

    challenge finalizado: Se agrega documentación con swagger

--- src/documents/swagger.js
+++ src/documents/swagger.js
@@ -1,24 +1,12 @@
 import swaggerJSDoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
-
-const options = {
-  definition: {
-    pinflagApi: '1.0.0',
-    info: {
-      title: 'Pinflag-Challenge',
-      version: '1.0.0'
-    }
-  },
-  apis: ['src/routes/index.js', 'src/config/sequelize.js']
-}
-
-const swaggerSpec = swaggerJSDoc(options)
+import options from './openapi.json'
 
 const swaggerDocs = (app, port) => {
-  app.use('/character/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+  app.use('/character/docs', swaggerUi.serve, swaggerUi.setup(options))
   app.get('/character/docs.js', (req, res) => {
     res.setHeader('Content-Type', 'application/json')
-    res.send(swaggerSpec)
+    res.send(options)
   })
   console.log(`Version 1 Docs are avaible at http://127.0.0.1:${port}/character/docs`)
 }
